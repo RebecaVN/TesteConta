71a6fb73827f06ba17cddcfd35fdb2b069359ee8
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-12 08:31:03 -0300

    Add files via upload

---
+++ TesteConta2.java
@@ -0,0 +1,14 @@
+package Atividade4;
+
+public class TesteConta2 {
+	public static void main(String[] args) {
+		
+		Conta2 conta1 = new Conta2(123, "Jerônimo Santos");
+		Conta2 conta2 = new Conta2(300, "Lais Moreira");
+		Conta2 conta3 = new Conta2(123, "Gilson Ribeiro");
+		Conta2 conta4 = new Conta2(300, "Alexandre Mattos");
+		
+	
+	
+	}
+}
